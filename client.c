ea77d0da1938eeeecbca5bde63df5bfbd4e3b7d2
jsebasti <sebastiagomezjosemanuel@gmail.com>
2022-12-28 07:37:57 +0000

    norminette bien

--- client.c
+++ client.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jsebasti <jsebasti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 19:17:49 by jsebasti          #+#    #+#             */
-/*   Updated: 2022/12/22 19:17:49 by jsebasti         ###   ########.fr       */
+/*   Updated: 2022/12/28 07:37:39 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void    client(int pid, const char *str)
+void	client(int pid, const char *str)
 {
-    
+	pid = 1;
 }
